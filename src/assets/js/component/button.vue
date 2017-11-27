63331229a9ab32b61a1d7001b90e49949560f376
Yuto Usui <usui@grageinc.com>
2017-11-28 02:49:52 +0900

    [add] button component

---
+++ src/assets/js/component/button.vue
@@ -0,0 +1,81 @@
+<template>
+    <button
+            class="c-button"
+            :class="[
+              type ? 'c-button--' + type : '',
+              size ? 'c-button--' + size : '',
+              {
+                'is-disabled': disabled,
+                'is-loading': loading,
+                'c-button--outline': outline,
+                'c-button--round': round,
+                'c-button--full': full
+              }
+             ]"
+            :disabled="disabled"
+            :type="buttonType"
+            :autofocus="autofocus"
+            @click="clickHandler"
+    >
+    <i
+            :class="[
+              'fa',
+              'fa-' + icon,
+            ]"
+            v-if="icon"
+    >
+    </i>
+    <slot></slot>
+    </button>
+</template>
+
+<script>
+  export default {
+
+    props: {
+      type: {
+        type: String,
+        default: ''
+      },
+      size: String,
+      icon: {
+        type: String,
+        default: ''
+      },
+      buttonType: {
+        type: String,
+        default: 'button'
+      },
+      loading: Boolean,
+      disabled: Boolean,
+      outline: Boolean,
+      autofocus: Boolean,
+      round: Boolean,
+      full: Boolean
+    },
+
+//    computed: {
+//      _elFormItemSize() {
+//        return (this.elFormItem || {}).elFormItemSize;
+//      },
+//      buttonSize() {
+//        return this.size;
+//        return this.size || this._elFormItemSize || (this.$ELEMENT || {}).size;
+//      }
+//    },
+
+    methods: {
+      clickHandler: function(e) {
+
+        if (this.disabled) {
+
+          return;
+
+        }
+        this.$emit('click', e);
+
+      }
+
+    }
+  };
+</script>
--- src/assets/js/main/main.js
+++ src/assets/js/main/main.js
@@ -1,13 +1,17 @@
 import 'babel-polyfill';
 import Vue from 'Vue';
 
+import favButton from 'Component/button';
 
-$('.main').addClass('is-active');
+// import __$ from './utility/selector';
+
+// $('.main').addClass('is-active');
 
 ////
 new Vue({
   el: '#app',
   components: {
+    favButton
   },
   data: {
     message: 'Hello,Vue.js!'
